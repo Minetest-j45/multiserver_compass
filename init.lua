34c84623b19e071595ec4101d5f97744248d48c9
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-03-21 10:50:19 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,10 +1,24 @@
 local multiserver_compass = {}
 
-local server_1_name = "Example"
-local server_1_img = "example.png"
+local server_1_name = "Example1"
+local server_1_img = "example1.png"
 
-multiserver_compass.fs = "size[4,4]" ..
-"image_button[1,1;1,1;" .. server_1_img";".. server_1_name ..";" .. server_1_name .. "]"
+local server_2_name = "Example2"
+local server_2_img = "example2.png"
+
+local server_3_name = "Example3"
+local server_3_img = "example3.png"
+
+local server_4_name = "Example4"
+local server_4_img = "example4.png"
+
+
+multiserver_compass.fs = "size[2,2]" ..
+"image_button[1,1;1,1;" .. server_1_img";".. server_1_name ..";" .. server_1_name .. "]" ..
+"image_button[1,1;1,2;" .. server_2_img";".. server_2_name ..";" .. server_2_name .. "]" ..
+"image_button[1,1;2,1;" .. server_3_img";".. server_3_name ..";" .. server_3_name .. "]" ..
+"image_button[1,1;2,2;" .. server_4_img";".. server_4_name ..";" .. server_4_name .. "]" ..
+
 
 minetest.register_craftitem("multiserver_compass:compass", {
 	description = "Multiserver compass",
